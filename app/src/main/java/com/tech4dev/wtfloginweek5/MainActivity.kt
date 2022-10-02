008284e6e1a1d6c2fa0959f84dd5c21639199617
emmilly immaculate <57020210+maqamylee0@users.noreply.github.com>
2022-10-02 20:44:33 +0300

    add connect views

--- app/src/main/java/com/tech4dev/wtfloginweek5/MainActivity.kt
+++ app/src/main/java/com/tech4dev/wtfloginweek5/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+      private lateinit var phoneNumber : EditText
+    private lateinit var button : Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+          phoneNumber = findViewById(R.id.phone)
+        button = findViewById(R.id.imageButton2)
     }
 }
